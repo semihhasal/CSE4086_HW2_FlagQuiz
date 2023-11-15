e525277ff578b11585721327f19becc169c5ff3e
Semihhasal <semih.hasal@htomail.com>
2023-11-15 09:43:15 +0300

    fisrt commit

---
+++ app/src/main/java/com/example/cse4086_hw2_flagquiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cse4086_hw2_flagquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
